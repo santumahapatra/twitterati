59c94384c9fabe2bf408936917b693be69d4559c
Santu Mahapatra <santu.m@multunus.com>
2014-12-11 15:15:19 +0530

    Adding validations for Tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ActiveRecord::Base
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe Tweet, :type => :model do
   let (:tweet) { Tweet.new }
 
   it { is_expected.to respond_to(:content) }
+
+  describe 'is invalid' do
+    it 'if content is blank' do
+      expect( Tweet.new(content: '') ).not_to be_valid
+    end
+
+    it 'if content is more than 140 characters' do
+      long_input = 'a' * 141
+      expect( Tweet.new(content: long_input) ).not_to be_valid
+    end
+  end
+
+  describe 'is valid' do
+    it 'if content is less than 140 characters' do
+      expect( Tweet.new(content: "Hello World") ).to be_valid
+    end
+  end
 end
